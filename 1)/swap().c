ae84f387f742d84420fb180772e3ac72ec7b790b
z2yeon <55427811+z2yeon@users.noreply.github.com>
2019-11-16 07:32:44 +0900

    swap()

---
+++ 1)/swap().c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define N_CARDSET			1
+#define N_CARD				52
+#define N_DOLLAR			50
+
+#define N_MAX_CARDNUM		13
+#define N_MAX_USER			5
+#define N_MAX_CARDHOLD		10
+#define N_MAX_GO			17
+#define N_MAX_BET			5
+
+#define N_MIN_ENDCARD		30
+
+void swap(int *a, int *b){
+	int temp;
+	
+	temp=*a;
+	*a=*b;
+	*b=temp;
+	
+}
